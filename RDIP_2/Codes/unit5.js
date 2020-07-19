66bef88a19510f3620cacdb2d27cdd8174ed0b64
sindhu070 <gorthi.17.it@anits.edu.in>
2020-07-19 14:21:11 +0530

    unit5 task2

---
+++ RDIP_2/Codes/unit5.js
@@ -0,0 +1,4 @@
+var english=['The child liked the chocolate.','She was stopped by the bravest knight.','Mary baked a cake for his birthday','She decorated the cake carefully','Mary wore a dress with polka dots.']
+var hindi=['राम ने सीता के लिए फल तोड़ा।','छोटे बच्चे पाठशाला जल्दी आयेंगे।','मेहनत का फल मीठा होता है।','वाह! वह खूबसूरत है।','पेड़ से पत्ते गिर गए']
+console.log(hindi)
+console.log(english)
